2495539bc053de4191e0b742b60da60d25c527d5
zhonghaoliu <joshualiu94@gmail.com>
2017-10-12 02:36:32 +0000

    commit: Template Engine - EJS

--- express_server.js
+++ express_server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -12,6 +14,21 @@ let urlDatabase = {
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id,
+                       longURL: urlDatabase[req.params.id] || 'Invalid URL!' };
+  res.render("urls_show", templateVars);
+});
+
+
+
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);

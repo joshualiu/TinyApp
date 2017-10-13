64efc18735526f2e820a2494ad3fcadfb79536c3
zhonghaoliu <joshualiu94@gmail.com>
2017-10-13 22:33:05 +0000

    clean up the code

--- express_server.js
+++ express_server.js
@@ -81,116 +81,96 @@ function urlsForUser(inputUser) {
   return urlsResult;
 }
 
-
-// Adding Routes -- GET:
+//// Adding Routes -- GET:
 app.get("/", (req, res) => {
-  if (users[req.session.user_id]) {
+  if (users[req.session.user_id]) {                 //if user is logged in
     res.redirect("/urls");
-  } else {
+  } else {                                          //if user is not logged in
     res.redirect("/login");
   }
 });
 
-
 app.get("/urls", (req, res) => {
   let user = users[req.session.user_id] || 0;
   if (user) {
-    let templateVars = { urls: urlsForUser(user),
-                         PORT: PORT,
-                         user: user};
+    let templateVars = { urls: urlsForUser(user), PORT: PORT, user: user };
     res.render("urls_index", templateVars);
   } else {
     res.end("<html><body><b>ERROR: You are not logged in!</b></body></html>\n");
   }
 });
 
-
-
-
-//only register and logged in users could access /urls/new
 app.get("/urls/new", (req, res) => {
-  if (users[req.session.user_id]) {
-    let templateVars = { user: users[req.session.user_id] || 0};
-    res.render("urls_new", templateVars);
-    return ;
+  let user = users[req.session.user_id] || 0;
+  if (user) {
+    res.render("urls_new", { user: user });
   } else {
-    res.redirect('/login');
-    return ;
+    res.redirect("/login");
   }
 });
 
-
 app.get("/urls/:id", (req, res) => {
-  let userCheck = users[req.session.user_id] || 0;
-  let URLexistCheck = urlDatabase[req.params.id] || 0;
+  let URLexistCheck = urlDatabase[req.params.id] || 0;    //check if a URL exist
   if (URLexistCheck === 0) {
     res.end("<html><body><b>ERROR: This short URL does not exist!</b></body></html>\n");
-    return;
+    return ;
   }
 
+  let userCheck = users[req.session.user_id] || 0;        //check if the user is logged in
   if (userCheck) {
-    let longURLCheck = urlDatabase[req.params.id]['fullURL'] || 0;
-    if (longURLCheck) {
-      if(userCheck.id == urlDatabase[req.params.id]['userID']) {
-        // console.log('user exist and logged in, short and long urls exist and match to the user!')
-        let templateVars = {shortURL: req.params.id,
-                            longURL: longURLCheck,
-                            user: userCheck };
+    let longURLCheck = urlDatabase[req.params.id]["fullURL"] || 0;
+    if (longURLCheck) {                                   //if the user is logged in, check if the long URL is matched to the user
+      if(userCheck.id == urlDatabase[req.params.id]["userID"]) {
+        let templateVars = {shortURL: req.params.id, longURL: longURLCheck, user: userCheck };
         res.render("urls_show", templateVars);
       } else {
-        // console.log('user exist and logged in, short and long urls exist but dont match to the user!')
         res.end("<html><body><b>ERROR:You are logged in, but you don't own the URL!</b></body></html>\n");
       }
     }
   } else {
     res.end("<html><body><b>ERROR: You are not logged in!</b></body></html>\n");
   }
-
 });
 
 app.get("/u/:id", (req, res) => {
-  let longURLCheck = urlDatabase[req.params.id] || 0;
-  if (longURLCheck) {
-    res.redirect(longURLCheck['fullURL']);
+  let shortURLCheck = urlDatabase[req.params.id] || 0;      //check if the short URL exist
+  if (shortURLCheck) {
+    res.redirect(shortURLCheck["fullURL"]);
   } else {
     res.end("<html><body><b>ERROR: The short URL does not exist!</b></body></html>\n");
   }
 });
 
-
-
 app.get("/login", (req, res) => {
-  if (users[req.session.user_id]) {
-    res.redirect('/urls');
+  let user = users[req.session.user_id] || 0;
+  if (user) {
+    res.redirect("/urls");
   } else {
-    res.render("login", { user: users[req.session.user_id] || 0});
+    res.render("login", { user: user });
   }
 });
-
 
 app.get("/register", (req, res) => {
-  if (users[req.session.user_id]) {
-    res.redirect('/urls');
+  let user = users[req.session.user_id] || 0;
+  if (user) {
+    res.redirect("/urls");
   } else {
-    res.render("register", { user: users[req.session.user_id] || 0});
+    res.render("register", { user: user });
   }
 });
 
 
-// Adding Routes -- POST:
-
+//// Adding Routes -- POST:
 app.post("/urls", (req, res) => {
   let POSTrequest = req.body.longURL;
-  // console.log(POSTrequest);  // debug statement to see POST parameters
   let tempShort = generateRandomString();
-  urlDatabase[tempShort] = { fullURL: POSTrequest, userID: req.session.user_id}
-  // urlDatabase[tempShort] = POSTrequest;
-  res.redirect("/urls");         // Respond with 'Ok' (we will replace this)
+  urlDatabase[tempShort] = { fullURL: POSTrequest, userID: req.session.user_id };
+  res.redirect("/urls");
 });
 
 app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id]['fullURL'] = req.body.longURL;
-  res.redirect('/urls');
+  urlDatabase[req.params.id]["fullURL"] = req.body.longURL;
+  res.redirect("/urls");
 });
 
 app.post("/urls/:id/delete", (req, res) => {
@@ -203,65 +183,54 @@ app.post("/login", (req, res) => {
   let curPassword = req.body.password;
 
   for (let key in users) {
-    if (users[key]['email'] === curEmail) {
-      if (bcrypt.compareSync(curPassword, users[key]['password'])) {
-        req.session.user_id = key;
-        // res.cookie("user_id", key);
-        res.redirect('/urls');
+    if (users[key]["email"] === curEmail) {                           //check if the emails are matched
+      if (bcrypt.compareSync(curPassword, users[key]["password"])) {  //check if the input password is matched with the encrypted password
+        req.session.user_id = key;                                    //set a cookie
+        res.redirect("/urls");
         return ;
       } else {
-        res.status(403).send("Wrong Password!");
+        res.status(403).send("Password did not match!");
         return ;
       }
     }
   }
-  res.status(403).send('Email address Not Found!');
-
+  res.status(403).send("Email address Not Found!");
 });
-
 
 app.post("/register", (req, res) => {
   let tempEmail = req.body.email;
   let tempPassword = req.body.password;
   let hashedPassword = bcrypt.hashSync(tempPassword, 10);
-  let emailList = [];
+  let emailList = [];                                                           //create a list to retrieve all email addresses, in order to check if an input email already exists
   for (let key in users) {
-    emailList.push(users[key]['email']);
+    emailList.push(users[key]["email"]);
   }
-  if (tempEmail == '' || tempPassword == '') {
-    res.status(400).send("Email or Password could not be empty :/");
+
+  if (tempEmail === '' || tempPassword === '') {
+    res.status(400).send("Email or Password could not be empty!");
     return ;
-  } else if (emailList.indexOf(tempEmail) != -1) {
-    res.status(400).send("Sorry, the email address has been used :/");
-    return;
+  } else if (emailList.indexOf(tempEmail) != -1) {                              //check if the input email already exists
+    res.status(400).send("Sorry, the email address has been used!");
+    return ;
   } else {
     let tempId = generateRandomString();
-    users[tempId] = {id: tempId, email: tempEmail, password: hashedPassword};
-    req.session.user_id = tempId;
-    // res.cookie("user_id", tempId);
-    // console.log(req.cookies);
+    users[tempId] = { id: tempId, email: tempEmail, password: hashedPassword }; //creates a new user
+    req.session.user_id = tempId;                                               //set a cookie
     res.redirect("/urls");
   }
 });
 
 app.post("/logout", (req, res) => {
   req.session = null;
-  // res.clearCookie("user_id");
-  res.redirect('/login');
+  res.redirect("/login");
 });
-
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`App listening on port ${PORT}!`);
 });
 
 

b9c17c44015b0ce53dc4d746ded94a14b422cfdc
zhonghaoliu <joshualiu94@gmail.com>
2017-10-13 21:29:38 +0000

    commit: functional requirements meet

--- express_server.js
+++ express_server.js
@@ -1,58 +1,22 @@
-require('dotenv').config();
-
+// Setup environment and libraries:
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
-
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+const cookieSession = require("cookie-session");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-// const cookieParser = require('cookie-parser');
-// app.use(cookieParser());
-
-const cookieSession = require('cookie-session')
-app.set('trust proxy', 1);
 app.use(cookieSession({
-  name:'session',
-  keys: ['user_id']
+  name:"session",
+  keys: ["user_id"]
 }));
 
 
-//Generate 6 random alphanumeric characters
-function generateRandomString() {
-  let text = '';
-  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let length = possible.length;
-  for (let i = 0; i < 6; i++) {
-    text += possible.charAt(Math.floor(Math.random() * length));
-  }
-  return text;
-}
-
-
-//global objects to save data:
-// let urlDatabase_old = {
-//   "b2xVn2": "http://www.lighthouselabs.ca",
-//   "9sm5xK": "http://www.google.com",
-//   "abcdef": "http://www.youtube.com",
-//   "123456": "http://www.facebook.com"
-// };
-
-//update urldatabase -- urls belong to users
-
-
-// let urlDatabase = {
-//   "josh": [{"b2xVn2": "http://www.lighthouselabs.ca"},
-//            {"9sm5xK": "http://www.google.com"}],
-//   "user1": [{"abcdef": "http://www.youtube.com"}],
-//   "test": [{"123456": "http://www.facebook.com"}]
-// };
-
-
-let urlDatabase = {
+// Define global variables to save and retrieve data:
+const urlDatabase = {
   "b2xVn2": {
     fullURL: "http://www.lighthouselabs.ca",
     userID: "josh"
@@ -70,9 +34,6 @@ let urlDatabase = {
     userID: "test"
   }
 };
-
-
-//
 
 const users = {
   "user1": {
@@ -97,13 +58,36 @@ const users = {
   }
 };
 
-//GET---------------
 
+// Function: generate 6 random alphanumeric characters:
+function generateRandomString() {
+  let text = '';
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let len = possible.length;
+  for (let i = 0; i < 6; i++) {
+    text += possible.charAt(Math.floor(Math.random() * len));
+  }
+  return text;
+}
+
+// Function: retrieve URLs from database for an input user:
+function urlsForUser(inputUser) {
+  let urlsResult = {};
+  for (let key in urlDatabase) {
+    if (urlDatabase[key]["userID"] === inputUser.id) {
+      urlsResult[key] = urlDatabase[key]["fullURL"];
+    }
+  }
+  return urlsResult;
+}
+
+
+// Adding Routes -- GET:
 app.get("/", (req, res) => {
   if (users[req.session.user_id]) {
-    res.redirect('/urls');
+    res.redirect("/urls");
   } else {
-    res.redirect('/login');
+    res.redirect("/login");
   }
 });
 
@@ -116,46 +100,11 @@ app.get("/urls", (req, res) => {
                          user: user};
     res.render("urls_index", templateVars);
   } else {
-    res.end("<html><body>ERROR: You are not logged in!</body></html>\n");
+    res.end("<html><body><b>ERROR: You are not logged in!</b></body></html>\n");
   }
-})
-
-function urlsForUser(inputUser) {
-  const urlsResult = {};
-  for (let key in urlDatabase) {
-    if (urlDatabase[key]['userID'] === inputUser.id) {
-      urlsResult[key] = urlDatabase[key]['fullURL'];
-    }
-  }
-  return urlsResult;
-}
+});
 
 
-// app.get("/urls", (req, res) => {
-//   //test code
-//   let user = users[req.cookies["user_id"]] || 0;
-//   // let urlsObj = {};
-//   // for (let key in urlDatabase) {
-//   //   if (urlDatabase[key]['userID'] === user.id) {
-//   //     urlsObj[key] = urlDatabase[key]['fullURL'];
-//   //   }
-//   // }
-
-//   let templateVars = { urls: urlsForUser(user),
-//                        PORT: PORT,
-//                        user: user};
-//   res.render("urls_index", templateVars);
-// });
-
-
-
-
-
-
-// app.get("/urls/new", (req, res) => {
-//   let templateVars = { user: users[req.cookies["user_id"]]};
-//   res.render("urls_new", templateVars);
-// });
 
 
 //only register and logged in users could access /urls/new
@@ -175,7 +124,7 @@ app.get("/urls/:id", (req, res) => {
   let userCheck = users[req.session.user_id] || 0;
   let URLexistCheck = urlDatabase[req.params.id] || 0;
   if (URLexistCheck === 0) {
-    res.end("<html><body>ERROR: The shortURL does not exist!</body></html>\n");
+    res.end("<html><body><b>ERROR: This short URL does not exist!</b></body></html>\n");
     return;
   }
 
@@ -183,18 +132,18 @@ app.get("/urls/:id", (req, res) => {
     let longURLCheck = urlDatabase[req.params.id]['fullURL'] || 0;
     if (longURLCheck) {
       if(userCheck.id == urlDatabase[req.params.id]['userID']) {
-        console.log('user exist and logged in, short and long urls exist and match to the user!')
+        // console.log('user exist and logged in, short and long urls exist and match to the user!')
         let templateVars = {shortURL: req.params.id,
                             longURL: longURLCheck,
                             user: userCheck };
         res.render("urls_show", templateVars);
       } else {
-        console.log('user exist and logged in, short and long urls exist but dont match to the user!')
-        res.end("<html><body>ERROR:You are logged in, but you dont own the URL!</body></html>\n");
+        // console.log('user exist and logged in, short and long urls exist but dont match to the user!')
+        res.end("<html><body><b>ERROR:You are logged in, but you don't own the URL!</b></body></html>\n");
       }
     }
   } else {
-    res.end("<html><body>ERROR: You are not logged in!</body></html>\n");
+    res.end("<html><body><b>ERROR: You are not logged in!</b></body></html>\n");
   }
 
 });
@@ -204,7 +153,7 @@ app.get("/u/:id", (req, res) => {
   if (longURLCheck) {
     res.redirect(longURLCheck['fullURL']);
   } else {
-    res.end("<html><body>ERROR: The shortURL does not exist!</body></html>\n");
+    res.end("<html><body><b>ERROR: The short URL does not exist!</b></body></html>\n");
   }
 });
 
@@ -228,7 +177,8 @@ app.get("/register", (req, res) => {
 });
 
 
-//POST
+// Adding Routes -- POST:
+
 app.post("/urls", (req, res) => {
   let POSTrequest = req.body.longURL;
   // console.log(POSTrequest);  // debug statement to see POST parameters
